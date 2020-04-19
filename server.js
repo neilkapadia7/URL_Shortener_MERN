b34df024937fb8bb5eca88ed4fe68e5ce01caca5
neilkapadia7 <neilkapadia7@gmail.com>
2020-04-19 12:32:40 +0530

    Backend Complete

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
+connectDB();
+
 app.use(express.json({ extended: false }));
+
+app.use("/api", require("./routes/index"));
+app.use("/api/url", require("./routes/url"));
 
 const PORT = 5000 || process.env.PORT;
 

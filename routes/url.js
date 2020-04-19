ff1fd25d94c8a4f52e8efb0b40ca453c0e5d390c
neilkapadia7 <neilkapadia7@gmail.com>
2020-04-19 14:01:50 +0530

    Backend Complete

--- routes/url.js
+++ routes/url.js
@@ -7,7 +7,7 @@ const config = require("config");
 const Url = require("../models/Url");
 
 // POST /api/url/shorten
-router.get("/shorten", async (req, res) => {
+router.post("/shorten", async (req, res) => {
 	const { longUrl } = req.body;
 	const baseUrl = config.get("baseURL");
 
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.use("/api", require("./routes/index"));
+app.use("/", require("./routes/index"));
 app.use("/api/url", require("./routes/url"));
 
 const PORT = 5000 || process.env.PORT;
